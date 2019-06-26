25ebf99af029c98510a5356838c97a35274b5ad2
Matt Jadud <matt@jadud.com>
2019-06-26 16:25:11 -0400

    Fixed warning.
    rom/uart.h was deprecated.
    
    esp32/rom/uart.h is preferred.
    
    Part of a move to IDF4.

--- main/sketch.ino.cpp
+++ main/sketch.ino.cpp
@@ -17,7 +17,7 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include <rom/uart.h>
+#include <esp32/rom/uart.h>
 
 extern "C" {
   #include <driver/periph_ctrl.h>
